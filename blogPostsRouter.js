f6841380a750675a67ba95c0377b6cc3279e1ca1
Jeff Woleslagle <jwoleslagle@gmail.com>
2018-03-01 13:45:06 -0500

    Adding testing

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -23,27 +23,71 @@ BlogPosts.create(
 BlogPosts.create(
   'Lions and tigers and bears oh my', lorem(), 'Lefty Lil');
 
-// add endpoint for GET. It should call `BlogPosts.get()`
-// and return JSON objects of stored blog posts.
-// send back JSON representation of all blog posts
-// on GET requests to root
-
-
-// add endpoint for POST requests, which should cause a new
-// blog post to be added (using `BlogPosts.create()`). It should
-// return a JSON object representing the new post (including
-// the id, which `BlogPosts` will create. This endpoint should
-// send a 400 error if the post doesn't contain
-// `title`, `content`, and `author`
-
-
-// add endpoint for PUT requests to update blogposts. it should
-// call `BlogPosts.update()` and return the updated post.
-// it should also ensure that the id in the object representing
-// the post matches the id of the path variable, and that the
-// following required fields are in request body: `id`, `title`,
-// `content`, `author`, `publishDate`
-
-// add endpoint for DELETE requests. These requests should
-// have an id as a URL path variable and call
-// `BlogPosts.delete()`
+  // when the root of this router is called with GET, return
+  // all current ShoppingList items
+  router.get('/', (req, res) => {
+    res.json(BlogPosts.get());
+  });
+  
+  
+  // when a new shopping list item is posted, make sure it's
+  // got required fields ('name' and 'checked'). if not,
+  // log an error and return a 400 status code. if okay,
+  // add new item to BlogPosts and return it with a 201.
+  router.post('/', jsonParser, (req, res) => {
+    // ensure `name` and `budget` are in request body
+    const requiredFields = ['title', 'author', 'content'];
+    for (let i=0; i<requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    }
+    const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+    res.status(201).json(item);
+  });
+  
+  
+  // when DELETE request comes in with an id in path,
+  // try to delete that item from ShoppingList.
+  router.delete('/:id', (req, res) => {
+    BlogPosts.delete(req.params.id);
+    console.log(`Deleted blog posts item \`${req.params.id}\``);
+    res.status(204).end();
+  });
+  
+  // when PUT request comes in with updated item, ensure has
+  // required fields. also ensure that item id in url path, and
+  // item id in updated item object match. if problems with any
+  // of that, log error and send back status code 400. otherwise
+  // call `ShoppingList.update` with updated item.
+  router.put('/:id', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'author', 'content', 'id'];
+    for (let i=0; i<requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    }
+    if (req.params.id !== req.body.id) {
+      const message = (
+        `Request path id (${req.params.id}) and request body id `
+        `(${req.body.id}) must match`);
+      console.error(message);
+      return res.status(400).send(message);
+    }
+    console.log(`Updating blog post item \`${req.params.id}\``);
+    const updatedItem = BlogPosts.update({
+      id: req.params.id,
+      title: req.body.title,
+      author: req.body.author,
+      content: req.body.content
+    });
+    res.status(204).end();
+  })
+  
+  module.exports = router;
